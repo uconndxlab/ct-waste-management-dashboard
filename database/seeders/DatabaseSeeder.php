85d0bac87dfaa430d018ecaa08c2dd1dcc28bd14
Emma Adams <23emmaadams@gmail.com>
2025-03-12 15:33:37 -0400

    testing out making some towns lol

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
+use App\Models\Town;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +22,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Town::create(['name' => 'Example1', 'description' => 'This is an example town!']);
+        Town::create(['name' => 'Example2', 'description' => 'This is another example town.']);
     }
 }
