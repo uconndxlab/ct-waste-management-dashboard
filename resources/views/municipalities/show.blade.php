409470e8feba8a52cf70d3d9908620b484cc3e07
Emma Adams <23emmaadams@gmail.com>
2025-03-13 14:35:06 -0400

    added the data for each town

--- resources/views/municipalities/show.blade.php
+++ resources/views/municipalities/show.blade.php
@@ -3,20 +3,19 @@
 @section('title', $municipality->name)
 
 @section('content')
-    <p><strong>Bulky Waste:</strong> $ {{ $municipality->bulky_waste }}</p>
-    <p><strong>Recycling:</strong> ${{ $municipality->recycling }}</p>
-    <p><strong>Tipping Fees:</strong> ${{ $municipality->tipping_fees }}</p>
-    <p><strong>Admin Costs:</strong> ${{ $municipality->admin_costs }}</p>
-    <p><strong>Hazardous Waste:</strong> ${{ $municipality->hazardous_waste }}</p>
-    <p><strong>Contractual Services:</strong> ${{ $municipality->contractual_services }}</p>
-    <p><strong>Landfill Costs:</strong> ${{ $municipality->landfill_costs }}</p>
-    <p><strong>Total Sanitation Refuse:</strong> ${{ $municipality->total_sanitation_refuse }}</p>
-    <p><strong>Only Public Works:</strong> ${{ $municipality->only_public_works }}</p>
-    <p><strong>Transfer Station Wages:</strong> ${{ $municipality->transfer_station_wages }}</p>
-    <p><strong>Hauling Fees:</strong> ${{ $municipality->hauling_fees }}</p>
-    <p><strong>Curbside Pickup Fees:</strong> ${{ $municipality->curbside_pickup_fees }}</p>
-    <p><strong>Waste Collection:</strong> ${{ $municipality->waste_collection }}</p>
-
+    <p><strong>Bulky Waste:</strong> {{ $municipality->bulky_waste !== '' ? '$' . $municipality->bulky_waste : 'No data' }}</p>
+    <p><strong>Recycling:</strong> {{ $municipality->recycling !== '' ? '$' . $municipality->recycling : 'No data' }}</p>
+    <p><strong>Tipping Fees:</strong> {{ $municipality->tipping_fees !== '' ? '$' . $municipality->tipping_fees : 'No data' }}</p>
+    <p><strong>Admin Costs:</strong> {{ $municipality->admin_costs !== '' ? '$' . $municipality->admin_costs : 'No data' }}</p>
+    <p><strong>Hazardous Waste:</strong> {{ $municipality->hazardous_waste !== '' ? '$' . $municipality->hazardous_waste : 'No data' }}</p>
+    <p><strong>Contractual Services:</strong> {{ $municipality->contractual_services !== '' ? '$' . $municipality->contractual_services : 'No data' }}</p>
+    <p><strong>Landfill Costs:</strong> {{ $municipality->landfill_costs !== '' ? '$' . $municipality->landfill_costs : 'No data' }}</p>
+    <p><strong>Total Sanitation Refuse:</strong> {{ $municipality->total_sanitation_refuse !== '' ? '$' . $municipality->total_sanitation_refuse : 'No data' }}</p>
+    <p><strong>Only Public Works:</strong> {{ $municipality->only_public_works !== '' ? '$' . $municipality->only_public_works : 'No data' }}</p>
+    <p><strong>Transfer Station Wages:</strong> {{ $municipality->transfer_station_wages !== '' ? '$' . $municipality->transfer_station_wages : 'No data' }}</p>
+    <p><strong>Hauling Fees:</strong> {{ $municipality->hauling_fees !== '' ? '$' . $municipality->hauling_fees : 'No data' }}</p>
+    <p><strong>Curbside Pickup Fees:</strong> {{ $municipality->curbside_pickup_fees !== '' ? '$' . $municipality->curbside_pickup_fees : 'No data' }}</p>
+    <p><strong>Waste Collection:</strong> {{ $municipality->waste_collection !== '' ? '$' . $municipality->waste_collection : 'No data' }}</p>
 
     <a href="/municipalities">Back to Municipalities</a>
 @endsection
