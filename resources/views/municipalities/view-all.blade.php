b93b5fff610c118dd0885676d3ef8777e0cd3489
Emma Adams <23emmaadams@gmail.com>
2025-03-13 16:15:31 -0400

    added css for the main overview and the view-all municipalities

--- resources/views/municipalities/view-all.blade.php
+++ resources/views/municipalities/view-all.blade.php
@@ -2,18 +2,25 @@
 
 @section('title', 'Municipalities')
 
+@section('breadcrumbs')
+    <li class="breadcrumb-item active" aria-current="page">Municipalities</li>
+@endsection
+
 @section('content')
-    <h1>All Municipalities</h1>
-    <form action="{{ route('municipalities.all') }}" method="GET">
-        <input type="text" name="search" placeholder="Search Municipality">
-        <button type="submit">Search</button>
+    <h1 class="text-primary">All Municipalities</h1>
+    
+    <form action="{{ route('municipalities.all') }}" method="GET" class="mb-3">
+        <div class="input-group">
+            <input type="text" name="search" class="form-control" placeholder="Search Municipality">
+            <button type="submit" class="btn btn-primary">Search</button>
+        </div>
     </form>
 
-    <ol>
+    <div class="list-group">
         @foreach($municipalities as $municipality)
-            <li>
-                <a href="{{ route('municipalities.view', ['name' => $municipality->name]) }}">{{ $municipality->name }}</a>
-            </li>
+            <a href="{{ route('municipalities.view', ['name' => $municipality->name]) }}" class="list-group-item list-group-item-action">
+                {{ $municipality->name }}
+            </a>
         @endforeach
-    </ol>
+    </div>
 @endsection
